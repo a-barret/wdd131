2e8de51cff9c831784311cb7a288a20c1018978e
a-barret <aidenb360@gmail.com>
2025-02-12 13:35:10 -0700

    worked on blog part 2 and unit 3 ponder assignments

---
+++ unit3_ponder/arrays.js
@@ -0,0 +1,7 @@
+//  arrays.js
+const steps = ["one", "two", "three"];
+function listTemplate(step) {
+  return `<li>${step}</li>` //the html string made from step
+}
+const stepsHtml = steps.map(listTemplate); // use map to convert the list from strings to HTML
+document.querySelector("#myList").innerHTML = stepsHtml; // set the innerHTML
