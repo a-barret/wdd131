22350570204edfde293f91bb0699185af71720ea
a-barret <aidenb360@gmail.com>
2025-03-01 22:51:47 -0700

    reconnected with repository

--- unit4_ponder/es_modules/course.mjs
+++ unit4_ponder/es_modules/course.mjs
@@ -1,10 +1,10 @@
-// courses.js
+// modules.mjs
 const aCourse = {
     code: "CSE121b",
     name: "Javascript Language",
     sections: [
-        { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
-        { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}
+        { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T' },
+        { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A' }
     ],
 
     init: function () {
