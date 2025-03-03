a8b82090a6d5c13f1f3f8c0a7ea935430d08ed81
a-barret <aidenb360@gmail.com>
2025-03-03 16:40:05 -0700

    Worked on registration form step 2 and 3

--- register/register.js
+++ register/register.js
@@ -1,3 +1,51 @@
-function participantTemplate(count) {
-    return `<section class="participant${count}">`
-}
+let numberOfParticipants = 1;
+
+function ParticipantTemplate(count)
+{
+    return `<section class="participant${count}">
+      <p>Participant ${count}</p>
+      <div class="item">
+        <label for="fname"> First Name<span>*</span></label>
+        <input id="fname" type="text" name="fname" value="" required />
+      </div>
+      <div class="item activities">
+        <label for="activity">Activity #<span>*</span></label>
+        <input id="activity" type="text" name="activity" />
+      </div>
+      <div class="item">
+        <label for="fee">Fee ($)<span>*</span></label>
+        <input id="fee" type="number" name="fee" />
+      </div>
+      <div class="item">
+        <label for="date">Desired Date <span>*</span></label>
+        <input id="date" type="date" name="date" />
+      </div>
+      <div class="item">
+        <p>Grade</p>
+        <select>
+          <option selected value="" disabled selected></option>
+          <option value="1">1st</option>
+          <option value="2">2nd</option>
+          <option value="3">3rd</option>
+          <option value="4">4th</option>
+          <option value="5">5th</option>
+          <option value="6">6th</option>
+          <option value="7">7th</option>
+          <option value="8">8th</option>
+          <option value="9">9th</option>
+          <option value="10">10th</option>
+          <option value="11">11th</option>
+          <option value="12">12th</option>
+        </select>
+      </div>
+    </section>`;
+};
+
+function AddParticipant()
+{
+    numberOfParticipants += 1;
+    const newParticipantHTML = ParticipantTemplate(numberOfParticipants);
+    const addButton = document.querySelector("#add").insertAdjacentHTML("beforebegin", newParticipantHTML);
+};
+
+document.querySelector("#add").addEventListener("click", AddParticipant);
